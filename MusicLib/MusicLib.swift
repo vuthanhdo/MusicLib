0a6c806a4bf0b2d14a9245350749c4efa307effc
Cong Nguyen <nguyen.smartosc@central.tech>
2024-01-03 13:56:25 +0700

    Added sample code with test cases

---
+++ MusicLib/MusicLib.swift
@@ -0,0 +1,19 @@
+//
+//  MusicLib.swift
+//  MusicLib
+//
+//  Created by Đô Vũ Thành on 03/01/2024.
+//
+
+import Foundation
+
+public final class MusicLib {
+    let name = "MusicLib"
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
--- MusicLibTests/MusicLibTests.swift
+++ MusicLibTests/MusicLibTests.swift
@@ -10,27 +10,17 @@ import XCTest
 
 final class MusicLibTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    var musicLib: MusicLib!
+    
+    override func setUp() {
+        musicLib = MusicLib()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func testAdd() {
+        XCTAssertEqual(musicLib.add(a: 1, b: 1), 2)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testSub() {
+        XCTAssertEqual(musicLib.sub(a: 2, b: 1), 1)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
