1383f5f9e7651bb297f2f5de1681af103efa1233
Cong Nguyen <nguyen.smartosc@central.tech>
2024-01-03 10:49:42 +0700

    Initial project setup

---
+++ MusicLib/MusicLib.h
@@ -0,0 +1,18 @@
+//
+//  MusicLib.h
+//  MusicLib
+//
+//  Created by Đô Vũ Thành on 03/01/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MusicLib.
+FOUNDATION_EXPORT double MusicLibVersionNumber;
+
+//! Project version string for MusicLib.
+FOUNDATION_EXPORT const unsigned char MusicLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MusicLib/PublicHeader.h>
+
+
---
+++ MusicLibExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicLibExample
+//
+//  Created by Đô Vũ Thành on 03/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
